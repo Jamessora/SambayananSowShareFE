a8a72d65b3d96e5e52bddd9d87b474e85993a10e
Jamessora <cadaciojerico@gmail.com>
2023-10-20 18:37:10 +0800

    fix error with 404

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 // Read environment variables
-const apiBaseUrl = process.env.VITE_API_BASE_URL_LOCAL || 'http://localhost:3000';
+const apiBaseUrl = process.env.VITE_API_BASE_URL_LOCAL || 'https://sambayanansowshare.onrender.com';
 
 export default defineConfig({
   plugins: [react()],
