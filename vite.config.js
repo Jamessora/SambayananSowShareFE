ba9bacde23804d6596f88267bbe05239c96744ac
Jamessora <cadaciojerico@gmail.com>
2023-10-21 07:45:51 +0800

    try api fix

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 // Read environment variables
-const apiBaseUrl = process.env.VITE_API_BASE_URL_PROD || process.env.VITE_API_BASE_URL_LOCAL;
+const apiBaseUrl = process.env.NODE_ENV === 'production' ?
+  process.env.VITE_API_BASE_URL :
+  process.env.VITE_API_BASE_URL_LOCAL;
+  
 export default defineConfig({
   plugins: [react()],
   server: {
